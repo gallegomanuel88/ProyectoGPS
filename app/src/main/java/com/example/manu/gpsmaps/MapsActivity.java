ba48b576fc90dc5feb83e93c3fff4ac2a4978ee7
Manuel Alonso Rodrígez <Manuel Alonso Rodrgez>
2017-02-02 20:24:53 +0100

    Cordenadas recogidas en el codigo

--- app/src/main/java/com/example/manu/gpsmaps/MapsActivity.java
+++ app/src/main/java/com/example/manu/gpsmaps/MapsActivity.java
@@ -1,13 +1,17 @@
 package com.example.manu.gpsmaps;
 
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -15,10 +19,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
     public static final int LOCATION_REQUEST_CODE = 1;
+    private GoogleApiClient apiCliente;
+    private Location objetoLocalizacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,9 +101,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    @Override
+    protected void onStart() {
+        apiCliente.connect();
+        super.onStart();
+    }
 
     @Override
     public void onMapClick(LatLng latLng) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        objetoLocalizacion = LocationServices.FusedLocationApi.getLastLocation(apiCliente);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
 
     }
 }
