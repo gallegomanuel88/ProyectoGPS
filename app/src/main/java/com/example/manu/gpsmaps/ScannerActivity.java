3e9d20c2967a6b5f4f99496db1e4a6879f1fbddc
Manuel Alonso Rodrígez <Manuel Alonso Rodrgez>
2017-02-10 11:51:18 +0100

    Añadida petición de permisos de cámara para versiones de android a partir de 6.0

--- app/src/main/java/com/example/manu/gpsmaps/ScannerActivity.java
+++ app/src/main/java/com/example/manu/gpsmaps/ScannerActivity.java
@@ -1,6 +1,9 @@
 package com.example.manu.gpsmaps;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,14 +17,47 @@ public class ScannerActivity extends AppCompatActivity implements ZXingScannerVi
 
     private ZXingScannerView mScannerView;
 
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1 ;
     @Override
     public void onCreate(Bundle state) {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
         super.onCreate(state);
 
         mScannerView = new ZXingScannerView(this);
         setContentView(mScannerView);
+
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.CAMERA)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+
+            }
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+
+                }
+                return;
+            }
+        }
+    }
     @Override
     public void onResume() {
         super.onResume();
